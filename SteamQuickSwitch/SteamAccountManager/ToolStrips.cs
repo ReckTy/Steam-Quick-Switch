b00f729bf820c04819a0e304d4dde9f289131a38
Mattias Aldhagen <39988708+ReckTy@users.noreply.github.com>
2019-03-13 04:11:56 +0100

    Edited file-structure

---
+++ SteamQuickSwitch/SteamAccountManager/ToolStrips.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteamQuickSwitch
+{
+    public partial class Form1
+    {
+
+        private void exitSQSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
